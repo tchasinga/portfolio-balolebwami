0496ce604a5eae779ad17c5d9ce2ed02cef2ba9a
Tchasinga jacques <tchasingajacques@gmail.com>
2023-09-26 09:55:55 +0200

    new comit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,14 @@
 import React  from "react";
 import "../index.css";
+import htmlDisplayer from "../svg/animation_lmzeh78hHtmL5.json";
+import CssDisplayer from "../svg/animation_lmzezawxCss.json"
+import ReactDisplayer from "../svg/animation_lmzeo9y8REACTJS.json"
+import MongoDisplayer from "../svg/animation_lmzeubvqMongoDB.json"
+import NodesDisplayer from "../svg/animation_lmzesmz3NodeJs.json"
+import NextDisplyer  from  "../svg/animation_lmzex845NextJs.json"
+import DataCode from "../svg/animation_lmzevyexDATACODE.json"
+import JavaScriptDisplayer from "../svg/animation_lmzerb6bJavaScript.json"
+import Lottie from "lottie-react";
 
 
 function Skills() {
@@ -7,15 +16,31 @@ function Skills() {
         <div>
             <div className="px-32">
             <h1 className="text-2xl text-white font-bold mb-14 mt-14 flex justify-center">Experience</h1>
-             <div className="Skills">
-                <div className="">One</div>
-                <div className="">Two</div>
-                <div className="">Three</div>
-                <div className="">Four</div>
-                <div className="">Five</div>
-                <div className="">Six</div>
-                <div className="">Seven</div>
-                <div className="">Eight</div>
+             <div className="Skills  items-center">
+                <div className="Langueage">
+                    <Lottie animationData={htmlDisplayer} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={CssDisplayer} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={ReactDisplayer} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={MongoDisplayer} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={NodesDisplayer} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={NextDisplyer} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={DataCode} />
+                </div>
+                <div className="Langueage">
+                    <Lottie animationData={JavaScriptDisplayer} />
+                </div>
              </div>
             </div>
         </div>
