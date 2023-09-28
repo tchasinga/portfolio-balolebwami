a842b59558741c1fa18819c274cdddcf11a3bd58
Tchasinga jacques <tchasingajacques@gmail.com>
2023-09-28 10:42:25 +0200

    new commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -18,7 +18,7 @@ const Portfolio = () => {
                         options && options.map((getElement) => {
                            return (
                               <div key={getElement.id} className=" getmyPortfolio  flex w-full gap-3 mt-10">
-                                <img src={getElement.UrlImg} alt={getElement.title} className="w-2/4" srcset="" />
+                                <img src={getElement.UrlImg} alt={getElement.title} className="w-2/4 myImgPortfolio" srcset="" />
                                 <div className="">
                                     <h3 className="text-2xl font-bold text-blue-800">{getElement.title}</h3>
                                     <hr  className="w-10  p-1 mt-1 text-blue-800"/>
