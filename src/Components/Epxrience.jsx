f55d7e6fa95f7b211f1c10cce76a7ad9b95ac8ca
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-10 16:42:06 +0200

    My new commit Code

---
+++ src/Components/Epxrience.jsx
@@ -0,0 +1,21 @@
+const Languagues = () => {
+    return (
+        <>
+         <div className="px-32  mt-32">
+               <h1 className="flex justify-center text-2xl font-bold text-white">My Skills</h1>
+              <div className="getElementSkills">
+                <div className="">Java</div>
+                <div className="">Html</div>
+                <div className="">JavaScript</div>
+                <div className="">MongoDb</div>
+                <div className="">TailwindCss</div>
+                <div className="">Css</div>
+                <div className="">AWS</div>
+                <div className="">React</div>
+              </div>
+         </div>
+        </>
+    )
+}
+
+export default Languagues;
