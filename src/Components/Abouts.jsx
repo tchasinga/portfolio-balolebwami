f87e8764739a46dda6257991bf1143769e8533af
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-11 13:50:48 +0200

    new adding line

--- src/Components/Abouts.jsx
+++ src/Components/Abouts.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Lottie from 'lottie-react';
-import AnimationCode from '../Animation/animation_ln1qjn0wCodeHome.json';
-import Portfolio from './Portfolio';
-import '../index.css';
-import MySkills from './GetSkills';
+import React, { useState, useEffect } from "react";
+import Lottie from "lottie-react";
+import AnimationCode from "../Animation/animation_ln1qjn0wCodeHome.json";
+import Portfolio from "./Portfolio";
+import "../index.css";
+import MySkills from "./GetSkills";
 
 const Abouts = () => {
   const textOptions = [
@@ -11,7 +11,7 @@ const Abouts = () => {
     "I'm passionate about coding",
     "I'm passionate in React",
     "I'm passionate about MongoDB",
-    "I'm passionate about NextJs"
+    "I'm passionate about NextJs",
   ];
 
   const [displayTextIndex, setDisplayTextIndex] = useState(0);
@@ -23,7 +23,9 @@ const Abouts = () => {
       const timer = setInterval(() => {
         setTextVisible(false); // Start fade-out animation
         setTimeout(() => {
-          setDisplayTextIndex((prevIndex) => (prevIndex + 1) % textOptions.length);
+          setDisplayTextIndex(
+            (prevIndex) => (prevIndex + 1) % textOptions.length
+          );
           setTextVisible(true); // Start fade-in animation with the new text
         }, 500); // Delay for the fade-out animation (0.5 seconds)
       }, 3000); // Change text every 3 seconds
@@ -44,7 +46,9 @@ const Abouts = () => {
       <div className="font-poppins container font-sans static flex-col px-32 mt-32 w-full gap-14">
         <div className="mainH1">
           <h1
-            className={`text-5xl  font-bold text-white fade-in-animation ${isTextVisible ? 'show' : ''}`}
+            className={`text-5xl  font-bold text-white fade-in-animation ${
+              isTextVisible ? "show" : ""
+            }`}
           >
             <span className="text-2xl text-gray-700">{currentText}</span> <br />
             Full Stack Developer <br />
@@ -55,16 +59,25 @@ const Abouts = () => {
           <div className="w-full hover:-rotate-6 cursor-pointer">
             <Lottie animationData={AnimationCode} />
           </div>
-          
+
           <div className="flex-col w-full">
             <h3 className="mb-4 text-2xl text-white">About me...</h3>
             <p className="text-gray-500 text-sm font-sans">
-              I'm a Full Stack Developer with a passion for learning new technologies and<br />
-              Developing new projects. I'm currently looking for a job as a Full Stack Developer.
-              Where I can learn more about the field and improve<br />My skills.
-              I will be happy to work with you.
-              I've strong skills in HTML, CSS, JavaScript, NextJs, <br />Ruby on Rails , Java, React, Node.js, Express.js, MongoDB, MySQL, Git, GitHub, and Render.
-              I'll be overjoyed about working with you.
+              I'm a Full Stack Developer with a passion for learning new
+              technologies and
+              <br />
+              Developing new projects. I'm currently looking for a job as a Full
+              Stack Developer. Where I can learn more about the field and
+              improve
+              <br />
+              My skills. I will be happy to work with you. I've strong skills in
+              HTML, CSS, JavaScript, <strong>NextJs</strong>,{" "}
+              <strong>Ruby on Rails</strong>, <strong>Java</strong>,{" "}
+              <strong>React</strong>, <strong>Node.js</strong>,{" "}
+              <strong>Express.js</strong>, <strong>MongoDB</strong>,{" "}
+              <strong>MySQL</strong>, <strong>Git</strong>,{" "}
+              <strong>GitHub</strong>, and <strong>Render</strong>. I'll be
+              overjoyed about working with you.
             </p>
           </div>
         </div>
--- src/Components/GetSkills.jsx
+++ src/Components/GetSkills.jsx
@@ -1,52 +1,91 @@
-import Lottie from 'lottie-react';
-import '../index.css'
-import AnimationCode from '../assets/animation_fronend.json'
-import AnimationBackend from '../assets/animation_Backend.json'
-import AnimationCodeJson from '../assets/animation_ApiJson.json'
-import AnimationDatabse from '../assets/animation_Database.json'
-import Languagues from './Epxrience';
+import Lottie from "lottie-react";
+import "../index.css";
+import AnimationCode from "../assets/animation_fronend.json";
+import AnimationBackend from "../assets/animation_Backend.json";
+import AnimationCodeJson from "../assets/animation_ApiJson.json";
+import AnimationDatabse from "../assets/animation_Database.json";
+import Languagues from "./Epxrience";
 
-const MySkills =() => {
-    return (
-        <div className=''>
-           <div className="Service  mt-32 px-32">
-              <h1 className=" font-bold text-white text-2xl  flex justify-center">SERVICE</h1>
-              <hr className="w-10  p-1 mt-1 relative left-1/2 border-black "/>
+const MySkills = () => {
+  return (
+    <div className="">
+      <div className="Service  mt-32 px-32">
+        <h1 className=" font-bold text-white text-2xl  flex justify-center">
+          SERVICE
+        </h1>
+        <hr className="w-10  p-1 mt-1 relative left-1/2 border-black " />
 
-              <div className="GridElement">
-                 <div className="parent  p-3 relative  flex-col ">
-                 <Lottie className='w-2/4 h-2/4   mt-2' animationData={AnimationCode} />
-                  <h1 className='absolute top-0 left-56 mt-5  text-white font-bold text-1xl'>Front-end desing</h1>  
-                  <p className='mt-3'>In Front-end I really fuced on  framwork language such as <strong>React </strong>,
-                  <strong>NextJs</strong>, and <strong>VueJs</strong> <strong>Typescript</strong> and <strong>Tailwindcss</strong> 
-                  </p>                
-                 </div>
-                 <div className="parent  p-3 relative  flex-col ">
-                 <Lottie className='w-2/4 h-2/4   mt-2' animationData={AnimationBackend} />
-                  <h1 className='absolute top-0 left-56 mt-5  text-white font-bold text-1xl'>Back-end developer</h1>  
-                  <p className='mt-3'>In Back-end language such as <strong>NodeJs</strong>,
-                  <strong>Express</strong>, and <strong>Java ,</strong> <strong>Ruby</strong>
-                  </p>                
-                 </div>
-                 <div className="parent  p-3 relative  flex-col ">
-                 <Lottie className='w-2/4 h-2/4   mt-2' animationData={AnimationCodeJson} />
-                  <h1 className='absolute top-0 left-56 mt-5  text-white font-bold text-1xl'>Restful API</h1>  
-                  <p className='mt-3'>In API I really fuced on  side such as <strong>JSON</strong>,
-                  <strong>GraphSql</strong>, and <strong>Data-structure</strong>
-                  </p>                
-                 </div>
-                 <div className="parent  p-3 relative  flex-col ">
-                 <Lottie className='w-2/4 h-2/4   mt-2' animationData={AnimationDatabse} />
-                  <h1 className='absolute top-0 left-56 mt-5  text-white font-bold text-1xl'>Data-base</h1>  
-                  <p className='mt-3'>In Front-end I really fuced on language such as <strong>MySql, </strong>
-                  <strong>MongoDb</strong>, and <strong>PostgreSql ,</strong> <strong>Firebase</strong> and <strong>Sql</strong> 
-                  </p>                
-                 </div>
-              </div>
-           </div>
-           <Languagues />
+        <div className="GridElement">
+          <div className="parent  p-3 relative  flex-col ">
+            <div className="flex justify-center">
+              <Lottie
+                className="w-2/4 h-2/4   mt-2"
+                animationData={AnimationCode}
+              />
+              <h1 className="text-white flex justify-center items-center font-bold text-1xl">
+                Front-end desing
+              </h1>
+            </div>
+            <p className="mt-3">
+              In Front-end I really fuced on framwork language such as{" "}
+              <strong>React </strong>,<strong>NextJs</strong>, and{" "}
+              <strong>VueJs</strong> <strong>Typescript</strong> and{" "}
+              <strong>Tailwindcss</strong>
+            </p>
+          </div>
+          <div className="parent  p-3 relative  flex-col ">
+            <div className="flex justify-center">
+              <Lottie
+                className="w-2/4 h-2/4   mt-2"
+                animationData={AnimationBackend}
+              />
+              <h1 className="text-white flex justify-center items-center font-bold text-1xl">
+                Back-end developer
+              </h1>
+            </div>
+            <p className="mt-3">
+              In Back-end language such as <strong>NodeJs</strong>,
+              <strong>Express</strong>, and <strong>Java ,</strong>{" "}
+              <strong>Ruby</strong>
+            </p>
+          </div>
+          <div className="parent  p-3 relative  flex-col ">
+            <div className="flex justify-center">
+              <Lottie
+                className="w-2/4 h-2/4   mt-2"
+                animationData={AnimationCodeJson}
+              />
+              <h1 className="text-white flex justify-center items-center font-bold text-1xl">
+                Restful API
+              </h1>
+            </div>
+            <p className="mt-3">
+              In API I really fuced on side such as <strong>JSON</strong>,
+              <strong>GraphSql</strong>, and <strong>Data-structure</strong>
+            </p>
+          </div>
+          <div className="parent  p-3 relative  flex-col ">
+            <div className="flex justify-center">
+              <Lottie
+                className="w-2/4 h-2/4   mt-2"
+                animationData={AnimationDatabse}
+              />
+              <h1 className="text-white flex justify-center items-center font-bold text-1xl">
+                Database structure
+              </h1>
+            </div>
+            <p className="mt-3">
+              In Front-end I really fuced on language such as{" "}
+              <strong>MySql, </strong>
+              <strong>MongoDb</strong>, and <strong>PostgreSql ,</strong>{" "}
+              <strong>Firebase</strong> and <strong>Sql</strong>
+            </p>
+          </div>
         </div>
-     )
-}
+      </div>
+      <Languagues />
+    </div>
+  );
+};
 
 export default MySkills;
