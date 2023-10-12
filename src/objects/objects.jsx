d3dd224d97aad1863968bb1d6e6675e0b0138488
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-12 11:45:00 +0200

    new checking line

--- src/objects/objects.jsx
+++ src/objects/objects.jsx
@@ -1,33 +1,31 @@
-
-
-
+import projectOne from '/src/project/ProjectOne.png'
 
 const options =[
     {
         id : 1,
         title : "System Manager",
-        UrlImg : "https://images.pexels.com/photos/574087/pexels-photo-574087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        UrlImg : projectOne,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
     {
         id : 2,
         title : "System Manager",
-        UrlImg : "../src/project/ProjectOne.png",
+        UrlImg : projectOne,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
     {
         id : 3,
         title : "System Manager",
-        UrlImg : "../src/project/ProjectOne.png",
+        UrlImg : projectOne,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
     {
         id : 4,
         title : "System Manager",
-        UrlImg : "../src/project/ProjectOne.png",
+        UrlImg : projectOne,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
