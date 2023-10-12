80ff0621b207f8752bba87cb88482d9e0c97aeae
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-12 20:35:25 +0200

    new commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,17 +3,19 @@ import Github from '/src/img/3d-fluency-github-logo.png'
 import LinkedIn from '/src/img/3d-fluency-linkedin-logo.png'
 import Gmail from '/src/img/3d-fluency-gmail-logo.png'
 import WhatSapp from '/src/img/icons8-whatsapp-480.png'
+import Contactus from '/src/assets/animation_contactus.json'
+import Lottie from 'lottie-react'
 
 
 const Footer = () =>{
   return (
      <footer className='myfooter mt-32 px-32'>
-         <h1 className='flex justify-center text-3xl p-14 font-bold border'>CONTACT</h1>
-         <div className='flex getColomn mt-14 gap-10'>
-             <div className='flex flex-col border w-2/4 getWidth'>
+         <h1 className='flex justify-center  text-3xl p-14 font-bold'>CONTACT</h1>
+         <div className='flex getColomn justify-between mt-14'>
+             <div className='flex flex-col  w-full getWidth'>
                 <h3 className='text-white text-xl font-semibold'>For more information</h3>
                 <p className='myborder font-bold bg-slate-700 w-40 p-2 flex'>My Resume</p>
-                <p className='mt-6 border font-normal mb-4'>
+                <p className='mt-6 font-normal mb-4'>
                 I'm a software engineer based in Rwanda, Kigali specializing in building and occasionally Designing exceptional websites, applications, and everything in Between.
                 </p>
                 <div>
@@ -33,7 +35,7 @@ const Footer = () =>{
                         <li className='w-full'>
                             <a className='flex  items-center gap-2 ' target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/">
                                 <img className='w-9' src={Gmail}/>
-                                <p className='border'>tchasingajacques@gmail.com</p>
+                                <p className=''>tchasingajacques@gmail.com</p>
                             </a>
                         </li>
                         <li>
@@ -45,7 +47,9 @@ const Footer = () =>{
                     </ul>
                 </div>
              </div>
-             <div>Two</div>
+             <div className=''>
+                <Lottie  animationData={Contactus} className='myLottie'/>
+             </div>
          </div>
      </footer>
   )
