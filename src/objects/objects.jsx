ed0686c258d81e2b198c9bd84118e7fc544d8d76
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-12 10:56:16 +0200

    new more coomit

--- src/objects/objects.jsx
+++ src/objects/objects.jsx
@@ -13,21 +13,21 @@ const options =[
     {
         id : 2,
         title : "System Manager",
-        UrlImg : "./src/project/ProjectOne.png",
+        UrlImg : "../project/ProjectOne.png",
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
     {
         id : 3,
         title : "System Manager",
-        UrlImg : "./src/project/ProjectOne.png",
+        UrlImg : "../project/ProjectOne.png",
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
     {
         id : 4,
         title : "System Manager",
-        UrlImg : "./src/project/ProjectOne.png",
+        UrlImg : "../project/ProjectOne.png",
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
