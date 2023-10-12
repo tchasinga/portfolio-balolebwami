268219ef3727c6b01b311b1539ad08acbeb8b956
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-12 11:01:49 -0700

    new pushing

--- src/objects/objects.jsx
+++ src/objects/objects.jsx
@@ -1,4 +1,8 @@
-import projectOne from '/src/project/ProjectOne.png'
+import projectOne from '../project/DesignOne.png'
+import projectTwo from '../project/DesignTwo.png'
+import projectThree from '../project/DesignThree.png'
+import projectFour from '../project/DesignFour.png'
+import projectFive from '../project/DesignFive.png'
 
 const options =[
     {
@@ -11,14 +15,14 @@ const options =[
     {
         id : 2,
         title : "System Manager",
-        UrlImg : projectOne,
+        UrlImg : projectTwo,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
     {
         id : 3,
         title : "System Manager",
-        UrlImg : projectOne,
+        UrlImg : projectThree,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
@@ -26,6 +30,20 @@ const options =[
         id : 4,
         title : "System Manager",
         UrlImg : projectOne,
+        description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
+    },
+
+    {
+        id : 5,
+        title : "System Manager",
+        UrlImg : projectFour,
+        description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
+    },
+
+    {
+        id : 6,
+        title : "System Manager",
+        UrlImg : projectFive,
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
