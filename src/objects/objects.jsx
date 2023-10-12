461f7059725e4e1d9a2c6ac5f9f5a7958bedb2b2
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-12 11:31:52 +0200

    new edit

--- src/objects/objects.jsx
+++ src/objects/objects.jsx
@@ -6,7 +6,7 @@ const options =[
     {
         id : 1,
         title : "System Manager",
-        UrlImg : "/src/img/IMG_8669.jpg",
+        UrlImg : "https://images.pexels.com/photos/574087/pexels-photo-574087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
     },
 
