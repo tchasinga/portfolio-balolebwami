9bb860093463cc10ff20dd177ecbba8505c118f8
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-27 14:55:24 +0200

    good

--- src/objects/objects.jsx
+++ src/objects/objects.jsx
@@ -1,65 +1,69 @@
-import projectOne from '../project/DesignOne.png'
-import projectTwo from '../project/DesignTwo.png'
-import projectThree from '../project/DesignThree.png'
-import projectFour from '../project/DesignFour.png'
-import projectFive from '../project/DesignFive.png'
-import projectSix from '../project/ScreenshotStuff.png'
+import projectOne from "../project/DesignOne.png";
+import projectTwo from "../project/DesignTwo.png";
+import projectThree from "../project/DesignThree.png";
+import projectFour from "../project/DesignFour.png";
+import projectFive from "../project/DesignFive.png";
+import projectSix from "../project/ScreenshotStuff.png";
 
-const options =[
-    {
-        id : 1,
-        title : "Space Travel hub",
-        UrlImg : projectOne,
-        description : "Welcome to the Space Travelers' Hub! This is the ultimate destination for all space enthusiasts, travelers, and adventurers. Whether you're a seasoned astronaut, a curious explorer, or just someone with a passion for the cosmos, you've come to the right place. At the Space Travelers' Hub",
-        getView : "https://space-traveling.onrender.com/",
-        getGithub : "https://github.com/tchasinga/space-travelers"
-    },
+const options = [
+  {
+    id: 1,
+    title: "Space Travel hub",
+    UrlImg: projectOne,
+    description:
+      "Welcome to the Space Travelers' Hub! This is the ultimate destination for all space enthusiasts, travelers, and adventurers. Whether you're a seasoned astronaut, a curious explorer, or just someone with a passion for the cosmos, you've come to the right place. At the Space Travelers' Hub",
+    getView: "https://space-traveling.onrender.com/",
+    getGithub: "https://github.com/tchasinga/space-travelers",
+  },
 
-    {
-        id : 2,
-        title : "Paint Art",
-        UrlImg : projectTwo,
-        description : "Painting is a visual art form that involves the application of pigments or other coloring agents to a surface, such as canvas, paper, or wood, to create an image or design. Paintings can be created using various techniques and styles, and they have been a significant form of artistic",
-        getView : "https://tchasinga.github.io/API-capstone-project/dist/",
-        getGithub : "https://github.com/tchasinga/API-capstone-project"
-    },
+  {
+    id: 2,
+    title: "Paint Art",
+    UrlImg: projectTwo,
+    description:
+      "Painting is a visual art form that involves the application of pigments or other coloring agents to a surface, such as canvas, paper, or wood, to create an image or design. Paintings can be created using various techniques and styles, and they have been a significant form of artistic",
+    getView: "https://tchasinga.github.io/API-capstone-project/dist/",
+    getGithub: "https://github.com/tchasinga/API-capstone-project",
+  },
 
-    {
-        id : 3,
-        title : "Book store",
-        UrlImg : projectThree,
-        description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
-        getView : "https://book-storing.onrender.com/",
-        getGithub : "https://github.com/tchasinga/API-capstone-project"
-    },
+  {
+    id: 3,
+    title: "Book store",
+    UrlImg: projectThree,
+    description:
+      "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system.",
+    getView: "https://book-storing.onrender.com/",
+    getGithub: "https://github.com/tchasinga/API-capstone-project",
+  },
 
+  {
+    id: 4,
+    title: "Personal Blogs",
+    UrlImg: projectFour,
+    description:
+      "Is personal Data server that help you to get close of your own data or information security, and it can used as to-do list management",
+    getView: "https://personalsblogsdata.netlify.app/",
+    getGithub: "https://github.com/tchasinga/personal-blogs",
+  },
 
-    {
-        id : 4,
-        title : "Personal Blogs",
-        UrlImg : projectFour,
-        description : "Is personal Data server that help you to get close of your own data or information security, and it can used as to-do list management",
-        getView : "https://personalsblogsdata.netlify.app/",
-        getGithub : "https://github.com/tchasinga/personal-blogs"
-    },
+  {
+    id: 5,
+    title: "workours-management",
+    UrlImg: projectFive,
+    description:
+      "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system, uisng MERN stack",
+    getView: "https://workours-management.onrender.com/",
+    getGithub: "https://github.com/tchasinga/system-manager",
+  },
+  {
+    id: 6,
+    title: "Stuff-seller",
+    UrlImg: projectSix,
+    description:
+      "Stuff is a website that help you to get close of your own data or information security, and it can used as to-do list management",
+    getView: "https://stuff-seller.netlify.app/",
+    getGithub: "https://github.com/tchasinga/e-comerce-site",
+  },
+];
 
-    {
-        id : 5,
-        title : "workours-management",
-        UrlImg : projectFive,
-        description : "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system, uisng MERN stack",
-        getView : "https://workours-management.onrender.com/",
-        getGithub : "https://github.com/tchasinga/system-manager"
-    },
-    {
-        id : 6,
-        title : "Stuff-seller",
-        UrlImg : projectSix,
-        description : "Stuff is a website that help you to get close of your own data or information security, and it can used as to-do list management",
-        getView : "https://stuff-seller.netlify.app/",
-        getGithub : "https://github.com/tchasinga/e-comerce-site"
-    }
-
-]
-
-export default options; 
+export default options;
