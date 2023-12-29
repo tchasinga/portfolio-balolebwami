8812cca9b5e4fa62b4954a729c09ac27b552847b
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-29 17:21:32 +0200

    New code is added

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -1,20 +1,42 @@
 import './heros.scss'
 import OneImg from '../Pics/IMG_8669-fotor-bg-remover-2023122911453.png'
 import OneImg2 from '../Pics/scroll.png'
+import {motion} from 'framer-motion' 
+
+
+
+const textVaraint ={
+  initial: {
+    x:-500,
+    opacity:0
+  },
+
+animate: {
+    x:0,
+    opacity:1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    }
+  }
+}
 
 export default function Hero() {
   return (
     <div className='hero'>
       <div className="wrapper">
-      <div className='textContainer'>
-        <h2 className='uppercase'>tchasinga jack</h2>
-        <h1 className=''>Full-stack Web developer</h1>
-        <div className="buttons">
-          <button className="btn">Hire me</button>
-          <button className="btn">Download CV</button>
-        </div>
-        <img src={OneImg2} alt="" />
+      <motion.div className='textContainer' variants={textVaraint} initial="initial" animate="animate">
+        <motion.h2 className='uppercase' variants={textVaraint}>tchasinga jack</motion.h2>
+        <motion.h1 className='' variants={textVaraint}>Full-stack Web developer</motion.h1>
+        <motion.div className="buttons" variants={textVaraint}>
+          <motion.button className="btn" variants={textVaraint}>Hire me</motion.button>
+          <motion.button className="btn" variants={textVaraint}>Download CV</motion.button>
+        </motion.div>
+        <motion.img variants={textVaraint} src={OneImg2} alt="" />
+      </motion.div>
       </div>
+      <div className="slidingTextContainer">
+        Full Stack Web Developer and Web Designer... 
       </div>
       <div className="imageContainer">
         <img src={OneImg} alt="" />
