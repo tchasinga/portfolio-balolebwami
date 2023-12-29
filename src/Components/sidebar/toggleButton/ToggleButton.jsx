d3c0869de92eb6f2117d46f8e200d14d0d2873c1
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-29 14:38:01 +0200

    new code is added

--- src/Components/sidebar/toggleButton/ToggleButton.jsx
+++ src/Components/sidebar/toggleButton/ToggleButton.jsx
@@ -6,16 +6,17 @@ import { motion } from 'framer-motion';
 export default function ToggleButton({setOpen}) {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
-      <svg width="23" height="23" viewBox='0 0 23 23'> 
-        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
+   <svg width="50" height="33" viewBox='0 0 23 23'> 
+        <motion.path d="M4 18L20 18" strokeWidth="3" stroke="white" strokeLinecap="round" />
+        <motion.path d="M4 12L20 12" strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <motion.path d="M4 6L20 6" strokeWidth="3" stroke="white" strokeLinecap="round" />
       </svg>
+
     </button>
   )
 }
 
 ToggleButton.propTypes = {
-  setOpen: PropTypes.func.isRequired,
+  setOpen: PropTypes.func.isRequired, 
 };
  
