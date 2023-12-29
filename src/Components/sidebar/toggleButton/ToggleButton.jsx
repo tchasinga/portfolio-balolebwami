1c7744ad0107296abbdfcb02af984dddd66f9f76
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-29 14:48:18 +0200

    new code is added now

--- src/Components/sidebar/toggleButton/ToggleButton.jsx
+++ src/Components/sidebar/toggleButton/ToggleButton.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 export default function ToggleButton({setOpen}) {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
-   <svg width="50" height="33" viewBox='0 0 23 23'> 
+   <svg width="40" height="33" viewBox='0 0 23 23'> 
         <motion.path d="M4 18L20 18" strokeWidth="3" stroke="white" strokeLinecap="round" />
         <motion.path d="M4 12L20 12" strokeWidth="3" stroke="black" strokeLinecap="round" />
         <motion.path d="M4 6L20 6" strokeWidth="3" stroke="white" strokeLinecap="round" />
