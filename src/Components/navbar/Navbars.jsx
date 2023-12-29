78e7ed3a48b02c4e38bdfd7755ec705a59afebf3
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-29 12:09:02 +0200

    new code

--- src/Components/navbar/Navbars.jsx
+++ src/Components/navbar/Navbars.jsx
@@ -1,20 +1,27 @@
-import "./navbars.scss"
-import {Link} from "react-router-dom"
+import "./navbars.scss";
 import { FaGithub, FaLinkedin, FaInstagram, FaFacebook } from "react-icons/fa";
 
 export default function Navbars() {
   return (
     <div className="navbar">
-        {/* Side bar */}
-        <div className="wrapper">
-            <span>Tchasinga Dev</span>
-            <div className="social">
-                <Link to={`https://github.com/tchasinga`}><FaGithub /></Link>
-                <Link to={`https://www.linkedin.com/in/tchasingajacques/`}><FaLinkedin /></Link>
-                <Link to={`https://www.instagram.com/jacques_balolebwami/`}><FaInstagram/></Link>
-                <Link to={`https://www.facebook.com/jaques.balolebwami`}><FaFacebook/></Link>
-            </div>
+      {/* Side bar */}
+      <div className="wrapper">
+        <span>Tchasinga Dev</span>
+        <div className="social">
+          <a href="https://github.com/tchasinga" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/tchasingajacques/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://www.instagram.com/jacques_balolebwami/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.facebook.com/jaques.balolebwami" target="_blank" rel="noopener noreferrer">
+            <FaFacebook />
+          </a>
         </div>
+      </div>
     </div>
-  )
+  );
 }
