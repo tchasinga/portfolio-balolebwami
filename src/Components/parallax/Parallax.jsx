28106820253672b948c7401720b711bbefa7820f
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-30 11:21:11 +0200

    New coding is added

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -1,20 +1,31 @@
 import PropTypes from "prop-types";
 import "./parallax.scss";
-import imgGod from "../Pics/newone.jpg";
+// import imgGod from "../Pics/newone.jpg";
 import ImageBG from "../Pics/secondone.jpg";
-import {motion} from "framer-motion";
+import {motion , useScroll, useTransform} from "framer-motion";
+import { useRef } from "react";
 
 export default function Parallax({ type }) {
+
+    const ref = useRef();
+
+    const { scrollYProgress } = useScroll({
+      target: ref,
+      offset: ["start start", "end start"],
+    });
+  
+    const yBg = useTransform(scrollYProgress, [0, 1], [0, 200]); 
+    const yText = useTransform(scrollYProgress, [0, 1], ["50%", "500%"]);
+
   return (
     <div
       className="parallax"
+      ref={ref}
       style={{
-        background: `url(${type === "service" ? imgGod : ImageBG}) center no-repeat`,
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className="absolute top-0 left-0 w-full h-full bg-black backdrop-blur-sm bg-opacity-50 flex flex-col justify-center items-center">
-        <motion.h1>{type === "service" ? "My Skills" : "What I did.."}</motion.h1>
+        background: `url(${type === "service" ? ImageBG : ImageBG}) center no-repeat`, y: yText}}>
+
+      <div className="absolute top-0 left-0 w-full h-full bg-black backdrop-blur-3xl bg-opacity-60 flex flex-col justify-center items-center">
+        <motion.h1 style={{y : yBg}}>{type === "service" ? "My Skills" : "What I did.."}</motion.h1>
       </div>
     </div>
   );
