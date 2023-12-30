b3ea5985a820a40ed0827aec6baf224f851b2ac6
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-30 13:32:32 +0200

    new program is added

--- src/Components/services/Services.jsx
+++ src/Components/services/Services.jsx
@@ -1,19 +1,45 @@
-import Languagues from './Languagues'
-import './service.scss'
-import {motion} from "framer-motion"
+import Languagues from "./Languagues";
+import "./service.scss";
+import { motion } from "framer-motion";
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export default function Services() {
   return (
-    <motion.div className='service'>
-         <motion.div className="textContainer">
-            <p >
-                I focus on coding in those listed technology
-                <br />and deep practice
-            </p>
-            <hr />
-         </motion.div>
-         <Languagues />
+    <motion.div className="service">
+      <motion.div
+        className="textContainer"
+        variants={variants}
+        initial="initial"
+        whileInView='animate'
+      >
+        <p>
+          I focus on coding in those listed technology
+          <br />
+          and deep practice
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div  variants={variants}
+        initial="initial"
+        whileInView='animate'>
+        <Languagues />
+      </motion.div>
     </motion.div>
-  )
+  );
 }
