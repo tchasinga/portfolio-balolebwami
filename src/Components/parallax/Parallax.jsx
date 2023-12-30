789214534a6c770a44cfe808a7d5625d3d78fef8
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-30 10:41:17 +0200

    new code is added

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -1,23 +1,21 @@
 import PropTypes from "prop-types";
 import "./parallax.scss";
-import imgGod from "../Pics/NewOne.jpg";
+import imgGod from "../Pics/newone.jpg";
 import ImageBG from "../Pics/secondone.jpg";
+import {motion} from "framer-motion";
 
 export default function Parallax({ type }) {
   return (
     <div
       className="parallax"
       style={{
-        background: `url(${type === "services" ? imgGod : ImageBG}) center no-repeat`,
+        background: `url(${type === "service" ? imgGod : ImageBG}) center no-repeat`,
         backgroundSize: 'cover',
       }}
     >
       <div className="absolute top-0 left-0 w-full h-full bg-black backdrop-blur-sm bg-opacity-50 flex flex-col justify-center items-center">
-        <h1>{type === "service" ? "My Skills" : "What I did.."}</h1>
+        <motion.h1>{type === "service" ? "My Skills" : "What I did.."}</motion.h1>
       </div>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="starts"></div>
     </div>
   );
 }
