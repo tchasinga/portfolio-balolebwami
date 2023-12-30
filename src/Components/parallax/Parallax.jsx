26c85dd5e1c0382cd1f5b6111e31231bd5aaad13
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-30 09:59:19 +0200

    new commit is added

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -3,7 +3,14 @@ import './parallax.scss'
 
 export default function Parallax({type}) {
     return (
-        <div>
+        <div
+      className="parallax"
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}>
                 <h1>{type ==="service" ? "My Skills" : "What I did.."}</h1>
         </div>
     )
