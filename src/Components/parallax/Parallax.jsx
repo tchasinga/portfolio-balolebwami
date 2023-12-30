e468ff81428e7e61930f901e5da9420ddbdd972c
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-30 10:37:08 +0200

    new code is added

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -1,21 +1,27 @@
-import PropTypes from 'prop-types';
-import './parallax.scss'
+import PropTypes from "prop-types";
+import "./parallax.scss";
+import imgGod from "../Pics/NewOne.jpg";
+import ImageBG from "../Pics/secondone.jpg";
 
-export default function Parallax({type}) {
-    return (
-        <div
+export default function Parallax({ type }) {
+  return (
+    <div
       className="parallax"
       style={{
-        background:
-          type === "services"
-            ? "linear-gradient(180deg, #111132, #0c0c1d)"
-            : "linear-gradient(180deg, #111132, #505064)",
-      }}>
-                <h1>{type ==="service" ? "My Skills" : "What I did.."}</h1>
-        </div>
-    )
+        background: `url(${type === "services" ? imgGod : ImageBG}) center no-repeat`,
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="absolute top-0 left-0 w-full h-full bg-black backdrop-blur-sm bg-opacity-50 flex flex-col justify-center items-center">
+        <h1>{type === "service" ? "My Skills" : "What I did.."}</h1>
+      </div>
+      <div className="mountains"></div>
+      <div className="planets"></div>
+      <div className="starts"></div>
+    </div>
+  );
 }
 
 Parallax.propTypes = {
-    type: PropTypes.string.isRequired,
-}
+  type: PropTypes.string.isRequired,
+};
