148af0786e1442f8dfe18801b4079ae53c058c83
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-20 22:17:36 +0200

    new code

--- src/components/Project.js
+++ src/components/Project.js
@@ -149,6 +149,7 @@ function Project() {
       id:12,
       title: "Calculator",
       image: projectTwelve,
+      details:"A calculator is a machine which allows people to do math operations more easily. For example, most calculators will add, subtract, multiply, and divide. Some also do square roots, and more complex calculators can help with calculus and draw function graphs.",
     }
   ]);
 
