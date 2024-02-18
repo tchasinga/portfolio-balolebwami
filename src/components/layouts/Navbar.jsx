bd2256e0ed401910f614661b5b8e0bcfe6916e06
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-18 15:12:22 +0200

    new changes

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -95,7 +95,7 @@ function Navbar({ dark, data }) {
                 href="#hero" 
                 className="font-bold text-xl text-gray-700 dark:text-white"
                 onClick={() => setMobileMenuOpen(false)}>
-                Robert Migot
+                Tchasinga jack
               </a>
               <button
                 type="button"
