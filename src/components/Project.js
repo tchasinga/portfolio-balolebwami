e944572efc8332ee0a7a18f2843ed299a7853d10
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-10 10:43:15 +0200

    new soem good code

--- src/components/Project.js
+++ src/components/Project.js
@@ -157,6 +157,7 @@ function Project() {
     },
     {
       id: 13,
+      title: "React-native",
       image: projectThirtheen,
       details: "React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces. Use a little—or a lot. You can use React Native today in your existing Android and iOS projects or you can create a whole new app from scratch.",
       techStack: "Nextjs, TailwindCss, Framer-motions, animejs",
